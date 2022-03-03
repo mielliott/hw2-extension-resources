fcc4b873c3d77f2dc8c5444bc75a28727f7426ae
Michael Elliott <mielliott@ufl.edu>
2022-03-03 14:04:56 -0500

    Add save-messages.py

---
+++ save-messages.py
@@ -0,0 +1,89 @@
+# Pipes the output of "awscurl --verbose" into this script to print the HTTP request and
+# response to a file. Echos the response to stdout. For awscurl, be sure to redirect
+# stderr to stdout using 2>&1 (see example below).
+#
+# Usage:
+#   python save-requests.py FILE
+# e.g.,
+#   awscurl --verbose http://google.com 2>&1 | python ./save-requests.py messages.txt
+
+def get_raw_request(prepared_request):
+    from urllib.parse import urlsplit
+    headers = prepared_request.headers.copy()
+    headers["host"] = urlsplit(prepared_request.url).netloc
+    return "{} {} {}\n{}{}".format(
+        prepared_request.method,
+        prepared_request.path_url,
+        "HTTP/1.1",
+        "\n".join('{}: {}'.format(key, value) for key, value in headers.items()),
+        "\n\n" + prepared_request.body.decode("utf-8") if prepared_request.body != None else "")
+
+def get_raw_response(response):
+    from http import HTTPStatus
+    return "{} {} {}\n{}{}".format(
+        "HTTP/1.1",
+        response.status_code,
+        HTTPStatus(response.status_code).name,
+        "\n".join('{}: {}'.format(key, value) for key, value in response.headers.items()),
+        "\n\n" + response.content.decode("utf-8") if response.content != None else "")
+
+# We only need the response object because it contains the request object
+def print_request_and_response(response):
+    print("---REQUEST---")
+    print(get_raw_request(response.request))
+    print("\n---RESPONSE---")
+    print(get_raw_response(response))
+
+
+import sys
+import re
+import ast
+from urllib.parse import urlsplit
+from functools import reduce
+from collections import namedtuple
+
+lines = [line.rstrip() for line in sys.stdin.readlines()]
+
+def find(lines, pattern, fallback):
+    return next((match[1] for match in filter(None, (re.search(pattern, line) for line in lines))), fallback)
+
+request_index = next((i for (i, line) in enumerate(lines) if line.find("HEADERS+") >= 0), None)
+response_index = next((i for (i, line) in enumerate(lines) if line.find("RESPONSE+") >= 0), None)
+url = find(lines, "Request URL = ([^']*)", "")
+split_url = urlsplit(url)
+path_url = split_url.path + split_url.query + split_url.fragment
+
+assert request_index != None, "Couldn't find request headers in awscurl's output"
+assert response_index != None, "Couldn't find response headers in awscurl's output"
+
+request = {
+    "method": find(lines, r"CANONICAL REQUEST = ([^\\]*)", "GET"),
+    "url": url,
+    "path_url": path_url if path_url.startswith("/") else "/" + path_url,
+    "headers": ast.literal_eval(lines[request_index + 1]),
+    "body": None
+}
+request =  namedtuple("ReconstructedRequest", request.keys())(*request.values())
+
+if request.path_url == "": request.path_url = "/"
+
+response = {
+    "status_code": int(find(lines, "Response code: ([0-9]*)", None)),
+    "headers": ast.literal_eval(lines[response_index + 2]),
+    "content": bytes("\n".join(lines[response_index + 4:]), "utf-8"),
+    "request": request
+}
+response =  namedtuple("ReconstructedResponse", response.keys())(*response.values())
+
+# Echo response
+print(response.content.decode("utf-8"))
+
+# Append request and response to a file
+assert len(sys.argv) >=2, "You must specify a file to save the HTTP request and response to"
+assert len(sys.argv) == 2, "Too many arguments provided for " + sys.argv[0]
+
+file_name = sys.argv[1]
+with open(file_name, 'a') as file:
+    sys.stdout = file
+    print_request_and_response(response)
+    print()
